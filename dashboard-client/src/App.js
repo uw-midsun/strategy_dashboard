0460baf06920e0bc9114f9b350af33ddf73391f7
RenJieZheng <42659888+RenJieZheng@users.noreply.github.com>
2023-05-27 17:48:46 -0400

    frontend boilerplate

---
+++ dashboard-client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dashboard</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
